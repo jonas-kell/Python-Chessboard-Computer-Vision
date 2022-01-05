8e7c76ad6dc3c76b2d78340b110ad4cbbb102b20
jonas-kell <jonas-kell@web.de>
2022-01-05 14:03:00 +0100

    own test folder

--- code/tests/cameraTest.py
+++ code/tests/cameraTest.py
@@ -11,7 +11,7 @@ while True:
     ret, frame = vid.read()
 
     # Display the resulting frame
-    cv2.imshow("frame", frame)
+    cv2.imshow("camera", frame)
 
     # the 'q' button is set as the
     # quitting button you may use any
